cc706592deb427631f9a49e11a5908e92d936d51
KordianRybarczyk <250993@edu.p.lodz.pl>
2024-05-22 23:44:11 +0200

    added functions

--- Array.h
+++ Array.h
@@ -3,6 +3,11 @@
 #ifndef SIMPLEARRAY_ARRAY_H
 #define SIMPLEARRAY_ARRAY_H
 #define SIZE 10
+void enterValue(int *array);
+void showArray(int *array);
+int findMaxValue(int *array);
+int findMinValue(int *array);
+float averageValue(int *array);
 
 
 
